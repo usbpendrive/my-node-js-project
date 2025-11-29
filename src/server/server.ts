a28281cddc12352082d7664d9e4a8e3db6cbd9b4
usbpendrive <ferry.fernando@gmail.com>
2025-11-29 10:00:34 +0700

    Server side script

---
+++ src/server/server.ts
@@ -0,0 +1,23 @@
+import { createServer } from 'http';
+import express, { Express } from 'express';
+import { testHandler} from "./testHandler";
+import httpProxy from "http-proxy";
+import helmet from "helmet";
+
+const port = 5000;
+const expressApp: Express = express();
+const proxy = httpProxy.createProxyServer({
+    target: "http://localhost:5000",
+});
+
+expressApp.use(helmet());
+expressApp.use(express.json());
+expressApp.post("/test", testHandler);
+expressApp.use(express.static("static"));
+expressApp.use(express.static("node_modules/bootstrap/dist"));
+expressApp.use((req, resp) => proxy.web(req, resp));
+
+const server = createServer(expressApp);
+server.on('upgrade', (req, socket, head) => proxy.ws(req, socket, head));
+server.listen(port,
+    () => console.log(`HTTP server listening on ${port}!`));
